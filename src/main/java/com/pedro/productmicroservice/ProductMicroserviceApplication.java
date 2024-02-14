6b8822bfe482c457cff2f43f8098ee53f2a14305
pedroapalmao <alexpedro6@gmail.com>
2024-02-14 10:55:38 -0300

    Add config server

--- src/main/java/com/pedro/productmicroservice/ProductMicroserviceApplication.java
+++ src/main/java/com/pedro/productmicroservice/ProductMicroserviceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProductMicroserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductMicroserviceApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ProductMicroserviceApplication.class, args);
+  }
 
 }
