10d355f201f723b7168447fdd46c87ac997b4a50
pedroapalmao <alexpedro6@gmail.com>
2024-02-15 10:26:00 -0300

    Add actuator dependency

--- src/main/java/com/pedro/productmicroservice/controller/CategoryController.java
+++ src/main/java/com/pedro/productmicroservice/controller/CategoryController.java
@@ -1,19 +1,25 @@
 package com.pedro.productmicroservice.controller;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Getter
 @Setter
-@RequiredArgsConstructor
+@RefreshScope
 @RestController
 @RequestMapping("api/categories")
 public class CategoryController {
 
-  @Value("${app.test-prod}")
-  private final String testProd;
+  @Value("${app.test-prod:test}")
+  private String testProd;
+
+  @GetMapping("test-prod")
+  public String getTesProd() {
+    return this.testProd;
+  }
 }
